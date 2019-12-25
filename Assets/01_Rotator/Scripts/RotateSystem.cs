4ff4d4489803c60efe1814dfafea26fd812f0e58
Unknown <lywin.shunayte@gmail.com>
2019-12-25 21:11:28 +0900

    ECS Rotator test

---
+++ Assets/01_Rotator/Scripts/RotateSystem.cs
@@ -0,0 +1,31 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+
+public class RotateSystem : JobComponentSystem
+{
+    [BurstCompile]
+    // protected override void OnUpdate()
+    // {
+    //     Entities.ForEach((ref Rotate rotate, ref RotationEulerXYZ euler) => {
+    //         euler.Value.y += rotate.radiansPerSeconds * Time.DeltaTime;
+    //     });
+    // }
+
+    private struct RotateJob : IJobForEach<RotationEulerXYZ, Rotate>
+    {
+        public float deltaTime;
+
+        public void Execute(ref RotationEulerXYZ euler, ref Rotate rotate)
+        {
+            euler.Value.y += rotate.radiansPerSeconds * deltaTime;
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var job = new RotateJob { deltaTime = Time.DeltaTime };
+        return job.Schedule(this, inputDeps);
+    }
+}
