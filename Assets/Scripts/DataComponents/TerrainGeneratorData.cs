ad72b98dc62761a696bcbe50a9dc714160b6e6ae
Unknown <lywin.shunayte@gmail.com>
2019-12-26 21:52:36 +0900

    Basic flat terrain generator

---
+++ Assets/Scripts/DataComponents/TerrainGeneratorData.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+public struct TerrainGeneratorData : IComponentData
+{
+    public Entity prefab;
+    public float maxDistanceFromSpawner;
+    public float secondsBetweenSpawns;
+    public float secondsToNextSpawn;
+    // public int maxSpawnCount;
+    // public int spawnCount;
+    public float3 terrainSize;
+    public float3 nextSpawnPos;
+}
